91e02bad5175f66168df0354a6a3c652a40d6773
Angel Felipe Cano Perilla <canos184@gmail.com>
2026-07-16 01:02:08 -0500

    Ajustes

--- backend/models/order.py
+++ backend/models/order.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Float, Integer, DateTime, ForeignKey
+from sqlalchemy import Column, String, Float, Integer, DateTime, ForeignKey, Text
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
